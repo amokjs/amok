641b0fb343d953dba3a249404fec4d812bffecee
Kuvakin Sergey <kuvakin@gmail.com>
2015-04-22 23:02:24 +0300

    issue #68 support chromium

--- test/test_open.js
+++ test/test_open.js
@@ -1,19 +1,25 @@
 var amok = require('../');
 var test = require('tape');
 
-test('open chrome', function(t) {
-  var options = {
-    client: 'chrome',
-    url: 'about:blank'
-  };
+var browsers = ['chrome', 'chromium'];
 
-  var client = amok.open(options, function() {
-    t.ok(client.pid);
-    client.kill();
-  });
+for (var i = 0; i < browsers.length; i++) {
+  var browser = browsers[i];
 
-  client.on('close', function(code) {
-    t.equal(code, 0);
-    t.end();
+  test('open ' + browser, function (t) {
+    var options = {
+      client: browser,
+      url: 'about:blank'
+    };
+
+    var client = amok.open(options, function () {
+      t.ok(client.pid);
+      client.kill();
+    });
+
+    client.on('close', function (code) {
+      t.equal(code, 0);
+      t.end();
+    });
   });
-});
+}
